259c9086535b8f74f04c790726afc0d10e8c1337
Nerissa Dorlus <nerissadorlus@Tdas-Pro-3.local>
2022-04-21 11:57:56 -0400

    Chicken and the egg example with setTimeout

---
+++ async.js
@@ -0,0 +1,6 @@
+console.log('This comes first. 🥚 ')
+setTimeout(function(){
+    console.log('This is the middle...')
+}, 0)
+// console.log('This is the middle... ')
+console.log('This is the end. 🐣')
